f1c643b92b12dfeeb63d4fa2e47c2e4ced5989c8
dimadima1159 <72620312+dimadima1159@users.noreply.github.com>
2020-10-31 14:26:00 +0200

    Add files via upload

---
+++ 2(5) задание.py
@@ -0,0 +1,10 @@
+a=int(input('первое целое число:'))
+b=int(input('второе целое число:'))
+if b!=0 and a//b !=0:
+    print(a//b)
+elif b!=0 and a%b != 0:
+    print(a%b)
+else:
+    print('nelzya na nulll delit')
+    
+    
