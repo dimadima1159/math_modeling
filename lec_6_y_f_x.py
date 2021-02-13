7f3d18800c1c1597fb10800f284c55666a3a4329
dimadima1159 <72620312+dimadima1159@users.noreply.github.com>
2021-02-13 13:30:27 +0200

    Create lec_6_y_f_x.py

---
+++ lec_6_y_f_x.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def parabola_plotter(a=1,b=1,c=0,title='Parabola plotter'):
+  """ Рисователь парабол общего вида
+  На входе нужно указать коэффициенты уравнения параболы
+  """
+  x = np.arange(-10,10,0.01)
+  y = a*x**2 + b*x + c
+  plt.plot(x,y,label='my parabola')
+  plt.xlabel('coord - x')
+  plt.ylabel('coord - y')
+  plt.title(title)
+  plt.legend()
+  plt.show()
+parabola_plotter()
