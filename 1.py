6b5d75cf6762e2a39e57b9057c406c5788b4e1ce
dimadima1159 <72620312+dimadima1159@users.noreply.github.com>
2021-04-23 11:30:48 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
  
 # Определяем переменную величину
-frames = 365
+frames = 100
 seconds_in_year = 365 * 24 * 60 * 60
 years = 1
 t = np.linspace(0, years*seconds_in_year, frames)
@@ -14,7 +14,8 @@ def move_func(s, t):
     (x1, v_x1, y1, v_y1,
      x2, v_x2, y2, v_y2,
      x3, v_x3, y3, v_y3,
-     x4, v_x4, y4, v_y4) =s
+     x4, v_x4, y4, v_y4,
+     x5, v_x5, y5, v_y5) =s
   
     dxdt1 = v_x1
     dv_xdt1 = - G * m * x1 / (x1**2 + y1**2)**1.5
@@ -36,10 +37,16 @@ def move_func(s, t):
     dydt4 = v_y4
     dv_ydt4 = - G * m * y4 / (x4**2 + y4**2)**1.5
 
+    dxdt5 = v_x5
+    dv_xdt5 = - G * m * x5 / (x5**2 + y5**2)**1.5
+    dydt5 = v_y5
+    dv_ydt5 = - G * m * y5 / (x5**2 + y5**2)**1.5
+
     return (dxdt1, dv_xdt1, dydt1, dv_ydt1,
             dxdt2, dv_xdt2, dydt2, dv_ydt2,
             dxdt3, dv_xdt3, dydt3, dv_ydt3,
-            dxdt4, dv_xdt4, dydt4, dv_ydt4)
+            dxdt4, dv_xdt4, dydt4, dv_ydt4,
+            dxdt5, dv_xdt5, dydt5, dv_ydt5)
 
 
 # Определяем начальные значения и параметры
@@ -66,10 +73,16 @@ v_x40 = -24138
 y40 = 228 * 10**9
 v_y40 = 0
 
+x50 = 0
+v_x50 = -200 * 10 ** 3
+y50 = 2.99196 * 10 ** 11
+v_y50 = 0
+
 s0 = (x10, v_x10, y10, v_y10,
       x20, v_x20, y20, v_y20,
       x30, v_x30, y30, v_y30,
-      x40, v_x40, y40, v_y40)
+      x40, v_x40, y40, v_y40,
+      x50, v_x50, y50, v_y50)
 
 # Решаем систему диф. уравнений
 def solve_func(i, key):
@@ -83,6 +96,8 @@ def solve_func(i, key):
         y3 = sol[i, 10]
         x4 = sol[i, 12]
         y4 = sol[i, 14]
+        x5 = sol[i, 16]
+        y5 = sol[i, 18]
     else:
         x1 = sol[:i, 0]
         y1 = sol[:i, 2]
@@ -92,7 +107,9 @@ def solve_func(i, key):
         y3 = sol[:i, 10]
         x4 = sol[:i, 12]
         y4 = sol[:i, 14]
-    return ((x1, y1), (x2, y2), (x3, y3), (x4, y4))
+        x5 = sol[:i, 16]
+        y5 = sol[:i, 18]
+    return ((x1, y1), (x2, y2), (x3, y3), (x4, y4), (x5,y5))
 
 # Строим решение в виде графика и анимируем
 fig, ax = plt.subplots()
@@ -109,6 +126,9 @@ ball_line3, = plt.plot([], [], '-', color='lime')
 ball4, = plt.plot([], [], 'o', color='gold')
 ball_line4, = plt.plot([], [], '-', color='gold')
 
+ball5, = plt.plot([], [], 'o', color='magenta')
+ball_line5, = plt.plot([], [], '-', color='magenta')
+
 plt.plot([0], [0], 'o', color='y', ms=20)
 
 def animate(i):
@@ -124,13 +144,16 @@ def animate(i):
     ball4.set_data(solve_func(i, 'point')[3])
     ball_line4.set_data(solve_func(i, 'line')[3])
 
+    ball5.set_data(solve_func(i, 'point')[4])
+    ball_line5.set_data(solve_func(i, 'line')[4])
+
 ani = FuncAnimation(fig,
                     animate,
                     frames=frames,
                     interval=30)
 
 plt.axis('equal')
-edge = 2*x10
+edge = 4*x10
 ax.set_xlim(-edge, edge)
 ax.set_ylim(-edge, edge)
 
